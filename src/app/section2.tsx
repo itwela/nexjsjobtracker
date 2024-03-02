e5e072ecabc6692ed189f4372677e090e2e2e8d7
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-03-02 08:57:46 -0500

    putting scroll trigger on homepage

--- src/app/section2.tsx
+++ src/app/section2.tsx
@@ -8,6 +8,9 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import { LuMessagesSquare } from "react-icons/lu";
 import { SlEnvolopeLetter } from "react-icons/sl";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function HeroSectionTwo() {
 
@@ -21,15 +24,28 @@ export default function HeroSectionTwo() {
       defaults: {ease: 'power2.out', duration: 2},
       scrollTrigger: {
         trigger: trigger,
-        start: 'top center',
+        start: 'top 70%',
         end: 'bottom 80%',
-        scrub: true,
+        // scrub: true,
         markers: true,
       },
     })
 
-    tl2.from((".second-wrapper"),{
-      opacity: 0.2, // Set your desired opacity value here
+    tl2.from(("#madeby"),{
+      y:'-39',
+    })
+
+    tl2.from([
+      "#clbutton", 
+      "#fubutton", 
+      "#atbutton", 
+      "#tiredof", 
+      "#trackall", 
+      "#cardpic", 
+    ],{
+      opacity: 0,
+      y:'-39',
+      stagger: 0.418
     })
 
 
@@ -45,15 +61,16 @@ export default function HeroSectionTwo() {
     
     <div className="flex flex-col gap-5 place-items-center h-[50vh]">
 
-        <h1 className="text-center text-3xl sm:text-5xl bg-gradient-to-r from-main-w to-lprimary bg-clip-text text-transparent">
-          Made <em className="text-blue-500 pr-3">by </em> job seekers, <br />
+        <h1  id='madeby' className="text-center text-3xl sm:text-5xl bg-gradient-to-r from-main-w to-lprimary bg-clip-text text-transparent">
+            Made <em className="text-blue-500 pr-3">by </em> job seekers,
+           <br />
           <em className="text-blue-500 pr-3">for </em> job seekers
         </h1>
 
         {/* button group */}
         <div className="w-[90%] sm:w-[70%] flex flex-col sm:flex-row place-items-center sm:place-items-center place-content-center gap-4">
 
-            <span className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
+            <span id="clbutton" className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
                 <span>
                   <SlEnvolopeLetter/>
                 </span>
@@ -62,7 +79,7 @@ export default function HeroSectionTwo() {
                 </span>
             </span>
 
-            <span className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
+            <span id="fubutton" className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
                 <span>
                   <LuMessagesSquare/>
                 </span>
@@ -71,7 +88,7 @@ export default function HeroSectionTwo() {
                 </span>
             </span>
 
-            <span className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
+            <span id="atbutton" className="h-[50px] text-main-w/60 hover:text-main-w leading-[1em] bg-gradient-to-l from-dprimary to-lprimary outline outline-[1px] hover:outline-[3px] ease-in outline-main-w/40 px-4 rounded-full p-2 select-none flex gap-2 place-items-center">
                 <span>
                   <FaRegCheckCircle/>
                 </span>
@@ -83,7 +100,7 @@ export default function HeroSectionTwo() {
         </div>
 
         {/* tired of ..... */}
-        <span className="pt-5 w-[70%]">
+        <span id="tiredof" className="pt-5 w-[70%]">
             <h2 className="text-sm sm:text-md text-center hover:text-main-w text-main-w/60">
                 Tired of rewriting cover letters? What about crafting messages to 
                 follow up on applications? Cant keep track of the 
@@ -94,11 +111,11 @@ export default function HeroSectionTwo() {
     </div>
 
     <div className="h-[50vh] relative flex place-content-start place-items-center flex-col">
-        <h1 className="flex w-[70%] text-3xl pt-[3em] sm:pt-[0em]  md:text-5xl bg-gradient-to-r from-main-w to-lprimary bg-clip-text text-transparent">
+        <h1 id="trackall" className="flex w-[70%] text-3xl pt-[3em] sm:pt-[0em]  md:text-5xl bg-gradient-to-r from-main-w to-lprimary bg-clip-text text-transparent">
             Track all your applications in one place
         </h1>
 
-        <Card className="hidden md:flex absolute border-main-w/40 top-[35%] h-[200em] w-[80vw] bg-gradient-to-b from-gray-100/20 via-transparent to-transparent backdrop-blur-lg bg-opacity-75">
+        <Card id="cardpic" className="hidden md:flex absolute border-main-w/40 top-[35%] h-[200em] w-[80vw] bg-gradient-to-b from-gray-100/20 via-transparent to-transparent backdrop-blur-lg bg-opacity-75">
               <CardContent>
             
                   
