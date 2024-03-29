851a26b28f52cb9b6d90cedc881ec363a0f8472c
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-03-29 17:21:50 -0400

    added resume to mobile

--- src/app/components/secondHeader.tsx
+++ src/app/components/secondHeader.tsx
@@ -260,6 +260,11 @@ export default function Secondheader({ userdata, userauth, jobdata }: { userdata
 
                       <span className="font-bold py-4 ">Features</span>
 
+                      <Link className="p-1 w-full cursor-pointer  flex justify-between gap-4" href='/resume'>
+                        <p className=' truncate'>Resume</p>
+                        <span className=''><FaRegNewspaper className='' /></span>
+                      </Link>
+
                       <Link className="p-1 w-full  flex justify-between gap-4" href='/coverletter'>
                         <p className=' truncate'>Cover Letter</p>
                         <span className=''><FaRegNewspaper className='' /></span>
@@ -463,6 +468,11 @@ export default function Secondheader({ userdata, userauth, jobdata }: { userdata
                   <span className="flex flex-col gap-4 my-4">
 
                     <span className="font-bold py-4 ">Features</span>
+
+                    <Link className="p-1 w-full cursor-pointer  flex justify-between gap-4" href='/resume'>
+                      <p className=' truncate'>Resume</p>
+                      <span className=''><FaRegNewspaper className='' /></span>
+                    </Link>
 
                     <Link className="p-1 w-full  flex justify-between gap-4" href='/coverletter'>
                       <p className=' truncate'>Cover Letter</p>
