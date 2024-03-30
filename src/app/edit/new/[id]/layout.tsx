2c8b82a7a8b74e7c30f34c349610a0464b5f2875
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-03-30 06:22:36 -0400

    potential error handling for edit page

--- src/app/edit/new/[id]/layout.tsx
+++ src/app/edit/new/[id]/layout.tsx
@@ -6,6 +6,14 @@ import { Suspense } from "react";
 export default async function EditSWithData() {
     const jobdata = await getJobData();
 
+    if (!jobdata) {
+        return  (
+            <div className="w-screen h-screen flex place-items-center place-content-center bg-gray-200">
+                <span>We're sorry, but something went wrong. Please try refreshing the page</span>
+            </div>
+        )
+    }
+
     return (
         <>
             <EditJob jobdata={jobdata} />
